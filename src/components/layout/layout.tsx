7a5147aefc3d6edd1401d61951e3c61cefa827de
Maxim Pavlov <ifelseapps@gmail.com>
2020-07-03 16:28:54 +0300

    An initial layout was created

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import { Helmet } from 'react-helmet';
+import { cn } from '../../classname';
 import { IConfig } from '../../contracts/config';
 import { FromYaml } from '../../contracts/utils';
+import './colors.css';
+import './fonts.css';
+import './layout.css';
+
+const cnPage = cn('page');
 
 export const Layout: React.FC = ({ children }) => {
   const data = useStaticQuery<FromYaml<IConfig>>(graphql`
@@ -17,13 +24,16 @@ export const Layout: React.FC = ({ children }) => {
   const { title, description } = data.dataYaml.layout;
 
   return (
-    <div>
-      <header>
+    <div className={cnPage()}>
+      <Helmet>
+        <title>Max Pavlov</title>
+      </Helmet>
+      <header className={cnPage('header')}>
         <div>{title}</div>
         <div>{description}</div>
       </header>
       <main>{children}</main>
-      <footer>Footer</footer>
+      <footer className={cnPage('footer')}>Footer</footer>
     </div>
   );
 };
