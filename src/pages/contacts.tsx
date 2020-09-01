8a0144ab866f917607cff1af1570ca9f60cf71bf
Maxim Pavlov <ifelseapps@gmail.com>
2020-09-01 08:41:43 +0300

    Create empty pages

---
+++ src/pages/contacts.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage: React.FC = () => {
+  return (
+    <>
+      <p className='content__row'>Coming soon.</p>
+    </>
+  );
+};
+
+export default IndexPage;
