19750c370fcdb8301f21fbc0bca8db5ad6758d8b
Maxim Pavlov <ifelseapps@gmail.com>
2020-09-01 16:19:21 +0300

    Contacts page

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -3,7 +3,20 @@ import React from 'react';
 const IndexPage: React.FC = () => {
   return (
     <>
-      <p className='content__row'>Coming soon.</p>
+      <p className='content__row'>
+        The best way to contact me is to write an email to{' '}
+        <a href='m&#97;ilt&#111;&#58;%&#54;&#68;e%&#52;0ma%78&#105;mp&#97;&#37;7&#54;l&#111;v%2Es&#37;7&#53;'>
+          me&#64;m&#97;ximpavlov&#46;su
+        </a>
+        . Follow me on{' '}
+        <a href='https://www.twitter.com/ifelseapps' target='_blank'>
+          Twitter
+        </a>{' '}
+        and{' '}
+        <a href='https://www.facebook.com/ifelseapps' target='_blank'>
+          Facebook
+        </a>
+      </p>
     </>
   );
 };
