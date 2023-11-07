99adacafe1edbe6e19a48b100651b4a02f36c60d
Maxim Pavlov <ifelseapps@gmail.com>
2023-11-07 22:34:33 +0300

    Добавил сборку стилей

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,32 @@
-module.exports = (e11ty) => {
+const sass = require('eleventy-sass')
+const rev = require('eleventy-plugin-rev')
+
+module.exports = (config) => {
+  config.addPlugin(rev)
+  config.addPlugin(sass, {
+    compileOptions: {
+      permalink: function (contents, inputPath) {
+        return (data) =>
+          data.page.filePathStem.replace(/^\/scss\//, '/css/') + '.css'
+      },
+    },
+    sass: {
+      style: 'compressed',
+      sourceMap: false,
+      loadPaths: ['src/_includes/scss'],
+      includes: 'src/_includes/scss',
+    },
+    rev: true,
+  })
+
   return {
     dir: {
       input: 'src',
       output: 'dist',
+      includes: '_includes',
     },
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    templateFormats: ['html', 'md', 'njk'],
   }
 }
